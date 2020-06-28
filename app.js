bacf39eca068918a3eccf7a5278ff245e8eb0f7b
whassa <remi.graton@gmail.com>
2020-06-28 03:46:27 -0400

    Add a node js server

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+const port = 8000
+
+app.use(express.static(__dirname));
+app.get('/', (req, res) => res.sendFile('index.html', { root: __dirname }))
+app.get('/home', (req, res) => res.sendFile('index.html', { root: __dirname }))
+app.get('/projectTarget', (req, res) => res.sendFile('game.html', { root: __dirname }))
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
