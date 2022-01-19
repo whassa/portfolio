92f8286892511616dbc5d9f2df1dd698cab72647
whassa <remi.graton@gmail.com>
2022-01-19 16:25:17 -0500

    Add work section

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ app.set('view engine', 'spy'); // register the template engine
 app.use(express.static(__dirname));
 app.get('/', (req, res) => res.render('index.spy'));
 app.get('/home', (req, res) => res.render('index.spy'))
+app.get('/works', (req, res) => res.render('works.spy'))
+
 app.get('/projectTarget', (req, res) => res.render('game.spy'))
 app.use(function(req, res) {
 	res.render('index.spy');
